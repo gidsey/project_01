167cba9f290302faf704580d1a32c0ea975b3f48
chrisguy <chris@chrisguy.co>
2019-02-20 17:47:01 +0000

    reset game added

--- guessing_game.py
+++ guessing_game.py
@@ -8,58 +8,7 @@ For this first project we will be using Workspaces.
 NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
 
 """
-
-import random
-
-def start_game():
-    attempts = 0
-    highscores =[]
-    solution = random.randint(1, 10)
-    # random module info from https://www.pythonforbeginners.com/random/how-to-use-the-random-module-in-python
-    print("\n" + "------------------------------------" +"\n" + "Welcome to the number guessing game!" +"\n" "------------------------------------" +"\n")
-    print ("The answer is {}".format(solution))
-    print ("\n")
-
-    while True:
-        try:
-            guess = int(input("Pick a number between 1 and 10:  "))
-        except ValueError:
-            print ("You must enter a whole number, please try again.")
-            continue
-
-        if guess < 1 or guess > 10:
-            print("Your guess is outside the range. Please try again.") 
-            continue
-        elif guess < solution:
-            # Answer too high
-            attempts +=1
-            print("It's higher !") 
-            continue
-        elif guess > solution:
-            # Answer too low
-            attempts +=1
-            print("It's lower !") 
-            continue
-        elif guess == solution:
-            #Correct answer given
-            attempts +=1
-            highscores.append(attempts)
-            if attempts == 1:
-                print ("Got it! It took you {} try.".format(attempts))
-            else: 
-                print ("Got it! It took you {} tries.".format(attempts))
-            restart = input("Would you like to play again [y]es/[n]o:  ")
-            if restart.lower()=="y":
-            #Python List min() method from https://www.tutorialspoint.com/python/list_min.htm
-                print("\n" + "The HIGHSCORE is {}".format(min(highscores)))
-                attempts = 0
-                continue
-            else:
-                print ("Closing game, see you next time!" + "\n")
-                break
-
-
-    """Psuedo-code Hints
+"""Psuedo-code Hints
     
     When the program starts, we want to:
     ------------------------------------
@@ -77,6 +26,57 @@ def start_game():
     """
     # write your code inside this function.
 
+
+import random
+
+def start_game():
+    attempts = 0
+    highscores =[]
+    solution = random.randint(1, 10)
+    # random module info from https://www.pythonforbeginners.com/random/how-to-use-the-random-module-in-python
+    print("\n" + "------------------------------------" +"\n" + "Welcome to the number guessing game!" +"\n" "------------------------------------" +"\n")
+
+    while True:
+        try:
+            guess = int(input("Pick a number between 1 and 10:  "))
+        except ValueError:
+            print ("You must enter a whole number, please try again.")
+            continue
+
+        if guess < 1 or guess > 10:
+            # Answer ouside range
+            print("Your guess is outside the range. Please try again.") 
+            continue
+        elif guess < solution:
+            # Answer too low
+            attempts +=1
+            print("It's higher!") 
+            continue
+        elif guess > solution:
+            # Answer too high
+            attempts +=1
+            print("It's lower!") 
+            continue
+        elif guess == solution:
+            #Correct answer given
+            attempts +=1
+            highscores.append(attempts)
+            if attempts == 1:
+                print ("Got it! It took you {} try.".format(attempts))
+            else: 
+                print ("Got it! It took you {} tries.".format(attempts))
+            restart = input("Would you like to play again [y]es/[n]o:  ")
+            if restart.lower()=="y":
+            #Restart the game
+            #Python List min() method from https://www.tutorialspoint.com/python/list_min.htm
+                print("\n" + "The HIGHSCORE is {}".format(min(highscores)))
+                #reset the solution and attempt count
+                solution = random.randint(1, 10)
+                attempts = 0
+                continue
+            else:
+                print ("Closing game, see you next time!" + "\n")
+                break
 
 
 if __name__ == '__main__':
